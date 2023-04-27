9c47090359729af8cb4f458218faa006b16fbc9e
J. B. Miles <jb_miles@berkeley.edu>
2023-04-27 15:53:57 -0700

    mid-lesson update

---
+++ class_2/in_class_output_operations.py
@@ -0,0 +1,17 @@
+X = 3
+Y = 7
+X += 2
+code = "hello"
+code += " world"
+month = "February"
+int_list = [1, 2, 3, 4, 5]
+veggie_list = ["celery", "carrot", "lettuce", "onion"]
+int_list.append(X)
+int_list.append(Y)
+print(int_list)
+veggie_list.insert(1, "bok choy")
+veggie_list.remove("carrot")
+print(veggie_list)
+print(2 in int_list)
+print("lettuce" in veggie_list)
+print("carrot" in veggie_list)
