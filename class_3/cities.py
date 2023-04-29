28ffd7f93f7e8db344f7d377fc5de5f381307611
J. B. Miles <jb_miles@berkeley.edu>
2023-04-29 16:40:56 -0700

    class draft complete 2023-04-29

---
+++ class_3/cities.py
@@ -0,0 +1,5 @@
+city_list = ["New York", "Los Angeles", "Chicago"]
+for city in city_list:
+    print(city)
+else:
+    print("Another city")
--- class_3/unit_converter.py
+++ class_3/unit_converter.py
@@ -1,8 +1,10 @@
 # Get the list of measurements from the user
-inches_list = input("Enter a list of measurements in inches, separated by commas: ").split(",")
+inches_list = input("Enter a list of measurements in inches separated by spaces: ").split(" ")
 
 # Convert the measurements to metric units
 for inches in inches_list:
-    inches = float(inches.strip())  # convert the string to a float and remove any whitespace
+    inches = float(inches)  # convert the string to a float
     centimeters = inches * 2.54    # convert the measurement to centimeters
     print(f"{inches:.2f} in. = {centimeters:.2f} cm.")
+else:
+    print("End of measurements.")
